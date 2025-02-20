2ed8f31411d26df2d35209fe5b67ec6cd9d51385
ziad-cpu <hziademp@gmail.com>
2025-02-19 19:31:59 -0500

    basic structure of the solar system

---
+++ src/test/java/SolarSystemB21/BasicStructure.java
@@ -0,0 +1,8 @@
+package SolarSystemB21;
+
+public class BasicStructure {
+
+    int sun;
+    int planets;
+    int moon;
+}
---
+++ src/test/java/SolarSystemB21/SolarSystem.java
@@ -0,0 +1,14 @@
+package SolarSystemB21;
+
+public class SolarSystem {
+    public static void main(String[] args) {
+
+
+        BasicStructure obrey =new BasicStructure();
+        obrey.sun=1;
+        obrey.moon=2;
+        obrey.planets=2;
+
+
+    }
+}
